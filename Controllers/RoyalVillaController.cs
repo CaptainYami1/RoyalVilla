8c42dacf99f36fdadb539b3f04d3f5ab7bbc5e10
gummybear6256 <loluadetemi@gmail.com>
2026-03-02 12:17:07 +0100

    day1

---
+++ Controllers/RoyalVillaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoyalVilla_API.Controllers
+{
+    [Route("villa")]
+    [ApiController]
+    public class RoyalVillaController : ControllerBase
+    {
+        [HttpGet]
+        public string GetVillas()
+        {
+            return "Get All Villas";
+        }
+
+        [HttpGet("GetVillasById{id}")]
+        public string GetVillasById(int id)
+        {
+            return ("Get Vila " + id );
+        }
+    }
+}
